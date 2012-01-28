59aa589c3a0267d83a0af2ce652806a6fca1fa54
Sebastian Probst Eide <sebastian.probst.eide@gmail.com>
2012-01-28 15:40:24 +0000

    Add file for PID controller

---
+++ QuadControl/PIDController/PIDController.h
@@ -0,0 +1,13 @@
+//
+//  PIDController.h
+//  QuadControl
+//
+//  Created by Sebastian Eide on 28.01.12.
+//  Copyright 2012 Kle.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PIDController : NSObject
+
+@end
---
+++ QuadControl/PIDController/PIDController.m
@@ -0,0 +1,23 @@
+//
+//  PIDController.m
+//  QuadControl
+//
+//  Created by Sebastian Eide on 28.01.12.
+//  Copyright 2012 Kle.io. All rights reserved.
+//
+
+#import "PIDController.h"
+
+@implementation PIDController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
